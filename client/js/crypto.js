29f52917d74d76f42af15a2e1466acef47d55043
Believe07 <blovebelieve@gmail.com>
2026-09-03 15:30:27 +0400

    Create crypto.js with planned cryptographic functions
    Add a placeholder for cryptographic functions with a plan.

---
+++ client/js/crypto.js
@@ -0,0 +1,12 @@
+/*
+ * Cryptographic functions will be implemented here.
+ *
+ * Planned:
+ *
+ * - ECDH key generation
+ * - Public key exchange
+ * - Shared secret derivation
+ * - HKDF
+ * - AES-GCM encryption
+ * - AES-GCM decryption
+ */
